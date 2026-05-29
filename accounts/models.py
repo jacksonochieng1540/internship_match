29214479831e91666ccae7c560f081547061f6bc
Jack ayaga <jacksonochieng1540@gmail.com>
2026-05-29 08:45:09 +0300

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -9,11 +9,10 @@ class User(AbstractUser):
 
     user_type = models.CharField(max_length=10, choices=USER_TYPE_CHOICES, default='student')
 
-    # Common fields for all users
+
     profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
     bio = models.TextField(blank=True, null=True)
 
-    # Company-specific fields
     company_name = models.CharField(max_length=255, blank=True, null=True)
     company_website = models.URLField(blank=True, null=True)
 
